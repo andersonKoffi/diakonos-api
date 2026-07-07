8a0250cd2e410e1f5d0a627447372e97337e4306
andersonKoffi <koffiandersonnda@gmail.com>
2026-07-07 13:15:16 +0000

    feat(tenant): add nestjs-cls
    add request-scoped tenant context middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './core/prisma/prisma.module';
+import { ClsModule } from 'nestjs-cls';
+import { TenantModule } from './core/tenant/tenant.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ClsModule.forRoot({
+      global: true,
+      middleware: {
+        mount: true,
+      },
+    }),
+    TenantModule,
     PrismaModule,
   ],
   controllers: [AppController],
---
+++ src/core/tenant/tenant.module.ts
@@ -0,0 +1,9 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TenantMiddleware } from './tenant.middleware';
+
+@Module({})
+export class TenantModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(TenantMiddleware).forRoutes('*');
+  }
+}
